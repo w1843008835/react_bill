9993c5d7462521f6297eb808c07c4b44ffb13f93
w1843008835 <chenyw1843@gmail.com>
2024-03-26 23:38:44 +0900

    commit

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -1,24 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+// 账单列表相关store
+
+import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
 const billStore = createSlice({
-    name: "bill",
+    name: 'bill',
+    // 数据状态state
     initialState: {
         billList: []
     },
     reducers: {
+        // 同步修改方法
         setBillList(state, action) {
             state.billList = action.payload
+        },
+        // 同步添加账单方法
+        addBill(state, action) {
+            state.billList.push(action.payload)
         }
     }
 })
 
-const { setBillList } = billStore.actions
+// 解构actionCreater函数
+const { setBillList, addBill } = billStore.actions
+// 编写异步
 const getBillList = () => {
     return async (dispatch) => {
-        const res = await axios.get("http://localhost:8888/ka")
+        // 编写异步请求
+        const res = await axios.get('http://localhost:8888/ka')
+        // 触发同步reducer
         dispatch(setBillList(res.data))
     }
 }
-export { getBillList }
+
+const addBillList = (data) => {
+    return async (dispatch) => {
+        // 编写异步请求
+        const res = await axios.post('http://localhost:8888/ka', data)
+        // 触发同步reducer
+        dispatch(addBill(res.data))
+    }
+}
+
+export { getBillList, addBillList }
+// 导出reducer
 const reducer = billStore.reducer
+
 export default reducer
